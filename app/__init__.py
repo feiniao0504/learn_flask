e103172a87e1eb20bcf794beefcc8c4ac4e6b1d8
zys <1181724859@qq.com>
2019-01-18 18:04:33 +0800

    hello world

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,29 @@
+import os
 from flask import Flask
+import logging
 
-app = Flask(__name__)
 
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="you will never guess",
+        DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
+    )
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        app.logger.info(app.instance_path)
+        return 'Hello, World!'
+
+    return app
