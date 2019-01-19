29db4df36b9b5129f4298f957990cd354818df38
zys <1181724859@qq.com>
2019-01-19 12:06:36 +0800

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, redirect, url_for, flash, render_template
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, request, redirect, url_for, flash, render_template, session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db import get_db
 
@@ -29,3 +29,26 @@ def register():
 
         flash(error)
     return render_template('auth/register.html')
+
+@bp.route('/login', method=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        db = get_db()
+        error = None
+
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username, )).fetchone()
+
+        if user is None:
+            error = 'Incorrect username'
+        elif not check_password_hash(user['password'], password)
+            error = 'Incorrect password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+    return render_template('auth/login.html')
